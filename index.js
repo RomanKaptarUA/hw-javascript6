269700f79d50e9f9d52e6fc7851e190648ce7bcb
RomanKaptarUA <romankaptar82@gmail.com>
2024-05-29 18:11:43 +0300

    a

--- index.js
+++ index.js
@@ -18,8 +18,25 @@ for (let i = 0; i <= 20; i += 1){
 const tab = 1;
 for (let i = 0; i <= 70; i += 7);
 // 4
+let ber = 1;
+while (ber > 6){
+    console.log('Дякую');
+}
 // 5
+let mas = 1;
+for (let i = 0; i <=10; i += 1){
+    if (i === 7){
+        break;
+    }
+}
 // 6
+let num = 20;
+for (let i = 0; i <=21; i += 1){
+if (i >= 20) {
+     break;
+}
+console.log('Помилка');
+}
 // 7
 
 let maxCounter = 1;
