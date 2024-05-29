f7246c535e6accc8afad001a61604f6fe46e959f
RomanKaptarUA <romankaptar82@gmail.com>
2024-05-29 17:55:14 +0300

    а

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1
+let clientsCounter = 1;
+
+
+ while (clientsCounter <= 10) {
+  console.log(clientsCounter);
+  clientsCounter += 1;
+ }
+//  2
+const sum = 2;
+for (let i = 0; i <= 20; i += 1){
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log('непарні');
+}
+// 3
+const tab = 1;
+for (let i = 0; i <= 70; i += 7);
+// 4
+// 5
+// 6
+// 7
+
+let maxCounter = 1;
+while (maxCounter <= 20) {
+    if (maxCounter % 3){
+          break;
+    }
+}
+
+
+
